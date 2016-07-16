d9e187b69c429e201fff94e153e867f876670501
DimuchGG <antibog8@gmail.com>
2016-07-17 00:51:02 +0300

    add List Frieng on custom_draw

--- app/src/main/java/com/nanddgroup/myapplication/MyAdapter.java
+++ app/src/main/java/com/nanddgroup/myapplication/MyAdapter.java
@@ -4,50 +4,46 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Dimuch on 16.07.2016.
  */
-public class MyAdapter extends BaseAdapter {
+public class MyAdapter extends ArrayAdapter<Friend> {
 
     Context ctx;
-    LayoutInflater lInflater;
-    ArrayList<Friend> objects;
+    ArrayList<Friend> alFriends;
 
-    MyAdapter(Context context, ArrayList<Friend> products) {
-        ctx = context;
-        objects = products;
-        lInflater = (LayoutInflater) ctx
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
-
-    @Override
-    public int getCount() {
-        return 0;
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
+    public MyAdapter(Context context, int resource, ArrayList<Friend> objects1) {
+        super(context, resource, objects1);
+        this.ctx = context;
+        alFriends = objects1;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        View view = convertView;
-        if (view == null) {
+        Toast.makeText(getContext(), ""+position, Toast.LENGTH_SHORT).show();
+        View view = null;
+        if (convertView == null) {
+            LayoutInflater lInflater = (LayoutInflater) ctx
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = lInflater.inflate(R.layout.custom_friend, parent, false);
+        } else {
+            view = convertView;
         }
+
+        TextView tvName = (TextView) view.findViewById(R.id.tvName);
+        tvName.setText(alFriends.get(position).getsName());
+        ImageView ivIcon = (ImageView) view.findViewById(R.id.ivIcon);
+        ivIcon.setImageResource(alFriends.get(position).getImageID());
 
         return view;
     }
