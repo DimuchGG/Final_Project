202e56c349d7bc592d5d7ef35578381d508d23c2
DimuchGG <antibog8@gmail.com>
2016-07-16 23:04:56 +0300

    init commit

---
+++ app/src/main/java/com/nanddgroup/myapplication/Friend.java
@@ -0,0 +1,19 @@
+package com.nanddgroup.myapplication;
+
+/**
+ * Created by Dimuch on 16.07.2016.
+ */
+public class Friend {
+
+    private String sName;
+    private int imageID;
+    private Boolean status;
+
+    public Friend(String sName, int imageID) {
+        this.sName = sName;
+        this.imageID = imageID;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/nanddgroup/myapplication/MyAdapter.java
@@ -0,0 +1,54 @@
+package com.nanddgroup.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Dimuch on 16.07.2016.
+ */
+public class MyAdapter extends BaseAdapter {
+
+    Context ctx;
+    LayoutInflater lInflater;
+    ArrayList<Friend> objects;
+
+    MyAdapter(Context context, ArrayList<Friend> products) {
+        ctx = context;
+        objects = products;
+        lInflater = (LayoutInflater) ctx
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = convertView;
+        if (view == null) {
+            view = lInflater.inflate(R.layout.custom_friend, parent, false);
+        }
+
+        return view;
+    }
+}
